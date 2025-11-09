4bc32ac156a997fd994a3fc659863acb38ac2214
Chirag Nikant Simepurushkar <simepurushkarchirag84@gmail.com>
2025-11-09 07:17:32 +0530

    url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1'; 
+const API_URL = 'https://server-campus-hive.vercel.app/api/v1' ||  'http://localhost:5000/api/v1'; 
 
 const api = axios.create({
   baseURL: API_URL,
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = 'https://server-campus-hive.vercel.app/' || import.meta.env.VITE_API_URL;
 
 // ============================================
 // GENERIC API UTILITIES
